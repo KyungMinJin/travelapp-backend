b003fa2575cc1def5371b5a8ccb6ac5626643611
KyungMinJin <kyung6479@korea.ac.kr>
2019-12-31 01:40:28 +0900

    jwt 쿠키 토큰 적용

--- src/api/auth/auth.ctrl.js
+++ src/api/auth/auth.ctrl.js
@@ -43,6 +43,12 @@ export const register = async ctx => {
     await user.save(); //저장
 
     ctx.body = user.serialize();
+
+    const token = user.generateToken();
+    ctx.cookies.set('access_token', token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true
+    });
   } catch (e) {
     ctx.throw(500, e);
   }
@@ -72,6 +78,12 @@ export const login = async ctx => {
       return;
     }
     ctx.body = user.serialize();
+
+    const token = user.generateToken();
+    ctx.cookies.set('access_token', token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true
+    });
   } catch (e) {
     ctx.throw(500, e);
   }
--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const UserSchema = new Schema({
   username: String,
@@ -26,6 +27,21 @@ UserSchema.methods.serialize = function() {
   const data = this.toJSON();
   delete data.hashedPassword;
   return data;
+};
+
+UserSchema.methods.generateToken = function() {
+  const token = jwt.sign(
+    //첫 파라미터에는 토큰 안에 넣고싶은 데이터 넣기
+    {
+      _id: this.id,
+      username: this.username
+    },
+    process.env.JWT_SECRET, //두번째로는 암호
+    {
+      expiresIn: '7d' //7일 유효
+    }
+  );
+  return token;
 };
 
 const User = mongoose.model('User', UserSchema);
